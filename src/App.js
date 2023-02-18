df59e2e439841ebb2771ddb63dc38a412f074f86
kazuki0517 <kh2328pkh@gmail.com>
2023-02-18 20:14:29 +0900

    add file for tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Title from './components/Title';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="test">
+      <h1>こんにちは</h1>
     </div>
   );
 }
