897857113bf83b7bca2630790a5738df3e77150d
kazuki0517 <kh2328pkh@gmail.com>
2023-02-18 21:50:41 +0900

    add contents

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,10 @@
-import { useState } from "react";
-import axios from "axios";
 
-const Form = () => {
- const [city, setCity] = useState("");
- const getWeather = (e) => {
-  e.preventDefault();
-  axios.get("http://api.weatherapi.com/v1/current.json?key=52b6ce11cb6b4f9c9b3113117231802&q=London&aqi=no").then(res => console.log(res))
- }
+
+const Form = (props) => {
  return (
   <form>
-   <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-   <button type="submit" onClick={getWeather}>Get Weather</button>
+   <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+   <button type="submit" onClick={props.getWeather}>Get Weather</button>
   </form>
  );
 };
